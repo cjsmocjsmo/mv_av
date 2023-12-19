a2c01c0b43484ec89d96094dc01b1491e76c4d13
Charlie J Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-12-18 22:57:16 -0800

    Add support for MPEG file extension in walk_dir function

--- src/walk_dirs.rs
+++ src/walk_dirs.rs
@@ -4,7 +4,7 @@ use walkdir::WalkDir;
 pub fn walk_dir(apath: String) -> Vec<String> {
     let mut keeper_vec = Vec::new();
     let mut idx = 0;
-    let ext_list = ["mp4", "MP4", "mov", "MOV", "avi", "AVI", "mkv", "MKV", "mpg", "MPG"];
+    let ext_list = ["mp4", "MP4", "mov", "MOV", "avi", "AVI", "mkv", "MKV", "mpg", "MPG", "mpeg", "MPEG"];
 
     for e in WalkDir::new(apath)
         .follow_links(true)
