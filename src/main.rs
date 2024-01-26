34236346435323f283237c03001b7ea5d71a2796
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-01-26 10:53:15 -0800

    Refactor video processing code

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,18 @@ use md5::compute;
 pub mod walk_dirs;
 
 fn main() {
-    let vp1 = "/home/teresa/Pictures/AllPics".to_string();
-    let vp2 = "/home/teresa/Pictures/PicCD1".to_string();
-    let vp3 = "/home/teresa/Pictures/PicCD2".to_string();
-    let vp4 = "/home/teresa/Pictures/PicCD3".to_string();
-    let vp5 = "/home/teresa/Pictures/PicISO".to_string();
-    let vp_list = vec![vp1, vp2, vp3, vp4, vp5];
-    for vid_path in vp_list {
-        let vid_out_path = "/home/teresa/Videos/AV/".to_string();
-        let vid_list = walk_dirs::walk_dir(vid_path);
-        for vid in vid_list {
-            let ext_split = vid.split(".").collect::<Vec<&str>>();
-            let ext = ext_split.last().unwrap();
-            let digest = compute(&vid);
-            let new_out_path = format!("{}{:?}.{}", vid_out_path, digest, ext);
-            println!("{} -> {}", vid, new_out_path);
-            std::fs::copy(vid, new_out_path).unwrap();
-        }
+    let vid_path = "/media/pi/taz/hpics_copy".to_string();
+    let vid_out_path = "/media/pi/taz/AV/".to_string();
+    let vid_list = walk_dirs::walk_dir(vid_path.clone());
+    for vid in vid_list {
+        let ext_split = vid.split(".").collect::<Vec<&str>>();
+        let ext = ext_split.last().unwrap();
 
+
+
+        // let digest = compute(&vid);
+        // let new_out_path = format!("{}{:?}.{}", vid_out_path, digest, ext);
+        // println!("{} -> {}", vid, new_out_path);
+        // std::fs::copy(vid, new_out_path).unwrap();
     }
 }
