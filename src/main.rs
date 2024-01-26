5e6c025954fe95768011e8b75dab9b61c8e574dc
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-01-26 11:06:20 -0800

    Add logic to filter out duplicate file extensions

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,17 @@ pub mod walk_dirs;
 fn main() {
     let vid_path = "/media/pi/taz/hpics_copy".to_string();
     let vid_out_path = "/media/pi/taz/AV/".to_string();
+    let mut ext_list = Vec::new();
     let vid_list = walk_dirs::walk_dir(vid_path.clone());
     for vid in vid_list {
         let ext_split = vid.split(".").collect::<Vec<&str>>();
-        let ext = ext_split.last().unwrap();
+        let ext = ext_split.last().unwrap().to_string();
+        if ext_list.contains(&ext) {
+            continue;
+        } else {
+            ext_list.push(ext.to_string());
+        }
+    print!("{:?}", ext_list)
 
 
 
