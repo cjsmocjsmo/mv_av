881b19c772a708abd768a4fbe2437c0af8220ac4
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-01-26 11:35:21 -0800

    Add dependencies and generate UUID for file names

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use md5::compute;
+use uuid::Uuid;
 pub mod walk_dirs;
 
 fn main() {
+    let uuid = Uuid::new_v4();
     let vid_path = "/media/pi/taz/hpics_copy".to_string();
     let vid_out_path = "/media/pi/taz/AV/".to_string();
     let vid_list = walk_dirs::walk_dir(vid_path.clone());
@@ -13,13 +15,15 @@ fn main() {
         // let new_file_path = format!("{}{}", vid_out_path, filename);
         // println!("{} ->\n {}", vid, new_file_path);
         
+        
     
 
 
 
-        let digest = compute(&vid);
-        let new_out_path = format!("{}{:?}.{}", vid_out_path, digest, ext);
-        println!("{} ->\n {}", vid, new_out_path);
+        // let digest = compute(&vid);
+        // let new_out_path = format!("{}{:?}.{}", vid_out_path, digest, ext);
+        let new_out_path2 = format!("{}{:?}.{}", vid_out_path, uuid, ext);
+        println!("{} ->\n {}", vid, new_out_path2);
         // std::fs::copy(vid, new_out_path).unwrap();
     }
     print!("{:?}", "Done!")
