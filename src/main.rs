52245113d338c67ef5b488d124846409cb8cabb5
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-01-26 11:38:31 -0800

    Remove unused code and generate UUID for each video file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use md5::compute;
+// use md5::compute;
 use uuid::Uuid;
 pub mod walk_dirs;
 
 fn main() {
-    let uuid = Uuid::new_v4();
     let vid_path = "/media/pi/taz/hpics_copy".to_string();
     let vid_out_path = "/media/pi/taz/AV/".to_string();
     let vid_list = walk_dirs::walk_dir(vid_path.clone());
     for vid in vid_list {
+        let uuid = Uuid::new_v4();
         let fname = vid.split("/").collect::<Vec<&str>>();
         let filename = fname.last().unwrap().to_string();
         let ext_split = filename.split(".").collect::<Vec<&str>>();
